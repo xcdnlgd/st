dfdd860274f0512f5be6cdf082e9c66978c2690e
xcdnlgd <xcdnlgd@hotmail.com>
2025-05-28 12:00:06 +0800

    PIXELSIZE macro

--- config.h
+++ config.h
@@ -1,15 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
+#define PIXELSIZE "24"
+
 /*
  * appearance
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMonoNL Nerd Font:regular:pixelsize=24:antialias=true:autohint=true";
-// static char *font = "JetBrainsMonoNL Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMonoNL Nerd Font:regular:pixelsize=" PIXELSIZE ":antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "Adwaita Mono:regular:pixelsize=24:antialias=true:autohint=true"
+    "Adwaita Mono:regular:pixelsize=" PIXELSIZE ":antialias=true:autohint=true"
     /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
     /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
